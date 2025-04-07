2b71abd249b2f34d527c798329cb4db941380ede
Mafatikhul281 <146699593+Mafatikhul281@users.noreply.github.com>
2025-04-07 12:28:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def check_win(row, col, player):
 import os
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
